3debbe947da8fe67fd849664a468ecb4466d1ddf
Sundeep Charan Ramkumar <72760920+sundeep-mavfarm@users.noreply.github.com>
2021-02-14 10:44:06 +0530

    Changed development port

--- app/src/index.js
+++ app/src/index.js
@@ -70,7 +70,7 @@ window.addEventListener("load", async function () {
     );
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:9545")
+      new Web3.providers.HttpProvider("http://127.0.0.1:7545") // Edited for my GANACHE CLI
     );
   }
 
